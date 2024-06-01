fdca4a512b9e1cc2c2cd699f5aea0aac8dc96c3b
Anton <a.chertok@akatech.com.ua>
2024-06-01 11:49:04 +0100

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { ReactComponent as ArrowIcon } from "assets/images/icons/arrow.svg";
+
+const App = () => {
+  return (
+    <div>
+      <h2>App is working...</h2>
+      <ArrowIcon />
+    </div>
+  );
+};
+
+export default App;
