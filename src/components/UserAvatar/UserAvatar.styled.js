a85a554ca5ca27a653d054542dc2b899712e4cf3
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2024-06-03 19:50:17 +0200

    Add fix styles  Avatar components

--- src/components/UserAvatar/UserAvatar.styled.js
+++ src/components/UserAvatar/UserAvatar.styled.js
@@ -1,8 +1,45 @@
 import styled from "styled-components";
+import theme from "styles/theme";
 
 export const Avatar = styled.img`
   border-radius: 50%;
   object-fit: cover;
-  width: ${props => props.size}px;
-  height: ${props => props.size}px;
+
+  ${theme.mq.mobile} {
+    width: ${props => {
+      switch (props.size) {
+        case 50:
+          return 32;
+        case 120:
+          return 80;
+        case 40:
+          return 32;
+        default:
+          return props.size;
+      }
+    }}px;
+
+    height: ${props => {
+      switch (props.size) {
+        case 50:
+          return 32;
+        case 120:
+          return 80;
+        case 40:
+          return 32;
+        default:
+          return props.size;
+      }
+    }}px;
+  }
+
+  ${theme.mq.tablet} {
+    width: ${props => props.size}px;
+    height: ${props => props.size}px;
+  }
+
+  ${theme.mq.desktop} {
+    width: ${props => props.size}px;
+    height: ${props => props.size}px;
+  }
 `;
