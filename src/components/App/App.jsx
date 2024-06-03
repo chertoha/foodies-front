44da05fcb9c2b8f82bd7ce6b05d0ad5aace6f494
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2024-06-03 23:07:49 +0200

    fix Routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,8 @@
-import axios from "axios";
 import { RouterProvider } from "react-router-dom";
 import router from "config/router";
-import Container from "components/Container";
-import { ReactComponent as ArrowIcon } from "assets/images/icons/arrow.svg";
-import { useEffect, useState } from "react";
 
 const App = () => {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    axios.get(`${process.env.REACT_APP_BASE_API_URL}/users/test`).then(response => {
-      setMessage(response?.data?.message);
-    });
-  }, []);
-
-  return (
-    <div>
-      <Container>
-        <h2>App is working...</h2>
-        <p>message: {message}</p>
-        <ArrowIcon />
-        TEST text
-      </Container>
-
-      <RouterProvider router={router} />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -5,7 +5,13 @@ const SharedLayout = () => {
   return (
     <>
       {/* <Header /> */}
+      <div>
+        <p>Header</p>
+      </div>
       <Outlet />
+      <div>
+        <p>Footer</p>
+      </div>
     </>
   );
 };
