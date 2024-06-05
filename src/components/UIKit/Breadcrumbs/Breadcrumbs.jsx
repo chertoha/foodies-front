e31ebf039fcd1f6af9461b12b334e62aa8d3ace7
Anton <a.chertok@akatech.com.ua>
2024-06-05 19:17:00 +0100

    Update Breadcrumbs.jsx

--- src/components/UIKit/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/UIKit/Breadcrumbs/Breadcrumbs.jsx
@@ -1,10 +1,7 @@
 import { ROUTES } from "config/router";
 import { BreadcrumbsWrapper, HomeLink } from "./Breadcrumbs.styled";
-import { useLocation } from "react-router-dom";
 
 const Breadcrumbs = ({ current }) => {
-  const location = useLocation();
-  console.log(location);
   return (
     <BreadcrumbsWrapper>
       <HomeLink to={ROUTES.HOME}>Home</HomeLink> / <p>{current}</p>
