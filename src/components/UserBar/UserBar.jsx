3983f8f0e08fb4174eee231899165bdc2895658d
dashuk <dashuk10@gmail.com>
2024-06-06 14:26:26 +0200

    add userBar + userBarMenu

---
+++ src/components/UserBar/UserBar.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import UserBarMenu from "../UserBarMenu/UserBarMenu";
+import UserAvatar from "../UserAvatar/UserAvatar";
+import IconsSprite from "../../img/sprite.svg";
+
+import { UserBarWrapper, UserName, WrappersIcons, Wrapper } from "./UserBar.styled";
+
+const UserBar = () => {
+  
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <UserBarWrapper>
+      <Wrapper>
+        <UserAvatar
+          size={[32, 50, 50]}
+          src={"https://ftp.goit.study/img/so-yummy/ingredients/640c2dd963a319ea671e37aa.png"}
+          alt={"avatar"}
+        />
+
+        <WrappersIcons onClick={toggleMenu}>
+          <UserName>Victoria</UserName>
+          {showMenu ? (
+            <svg
+              width="18"
+              height="18"
+              fill="#fff"
+            >
+              <use xlinkHref={`${IconsSprite}#icon-chevron-up`} />
+            </svg>
+          ) : (
+            <svg
+              width="18"
+              height="18"
+              fill="#fff"
+            >
+              <use xlinkHref={`${IconsSprite}#icon-chevron-down`} />
+            </svg>
+          )}
+        </WrappersIcons>
+      </Wrapper>
+
+      {showMenu && <UserBarMenu />}
+    </UserBarWrapper>
+  );
+};
+
+export default UserBar;
---
+++ src/components/UserBarMenu/UserBarMenu.styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { Link } from 'react-router-dom';
+
+export const UserBarMenuWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 6px;
+  width: 148px;
+  height: 74px;
+  background-color: #050505;
+  border-radius: 15px;
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  padding: 16px;
+`;
+
+export const UserBarMenuLink = styled(Link)`
+  color: #fff;
+  font-family: Mulish;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 18px;
+  letter-spacing: -0.24px;
+  text-transform: uppercase;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
