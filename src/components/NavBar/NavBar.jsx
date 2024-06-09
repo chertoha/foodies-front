5b44612789546bf86220917af1a4785b2a4ff24f
dashuk <dashuk10@gmail.com>
2024-06-09 18:03:44 +0200

    add NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,23 @@
-import { ROUTES } from "../../router";
+import React from "react";
 import { NavMenuWrapper, NavMenuLink } from "./NavBar.styled";
+import { ROUTES } from "config/router";
 
 const NavBar = ({ inverse }) => {
   return (
-    <>
-      <NavMenuWrapper>
-        <NavMenuLink
-          to={ROUTES.HOME}
-          inverse={inverse}
-        >
-          Home
-        </NavMenuLink>
-        <NavMenuLink
-          to={ROUTES.RECIPE_ADD}
-          inverse={inverse}
-        >
-          Add recipe
-        </NavMenuLink>
-      </NavMenuWrapper>
-    </>
+    <NavMenuWrapper>
+      <NavMenuLink
+        to={ROUTES.HOME}
+        inverse={inverse}
+      >
+        Home
+      </NavMenuLink>
+      <NavMenuLink
+        to={ROUTES.RECIPE_ADD}
+        inverse={inverse}
+      >
+        Add recipe
+      </NavMenuLink>
+    </NavMenuWrapper>
   );
 };
 
