996f0aa08b8e1ff8e0deab40a110fa782f02e3fa
dashuk <dashuk10@gmail.com>
2024-06-09 18:28:03 +0200

    add hot fix

--- src/components/MobileMenuIcon/MobileMenuIcon.jsx
+++ src/components/MobileMenuIcon/MobileMenuIcon.jsx
@@ -3,15 +3,17 @@ import IconsSprite from "../../img/sprite.svg";
 import { MobileMenuIconWrapper } from "./MobileMenuIcon.styled";
 
 const MobileMenuIcon = ({ toggleMenu }) => {
-  return <MobileMenuIconWrapper onClick={toggleMenu}>
-    <svg
-          width="18"
-          height="18"
-          fill="#fff"
-        >
-          <use xlinkHref={`${IconsSprite}#icon-mobile-menu`} />
-        </svg>
-  </MobileMenuIconWrapper>;
+  return (
+    <MobileMenuIconWrapper onClick={toggleMenu}>
+      <svg
+        width="18"
+        height="18"
+        fill="#fff"
+      >
+        <use xlinkHref={`${IconsSprite}#icon-mobile-menu`} />
+      </svg>
+    </MobileMenuIconWrapper>
+  );
 };
 
 export default MobileMenuIcon;
