03c9e4ae425f4eed25287e4b2b866e0e5d5f056d
dashuk <dashuk10@gmail.com>
2024-06-11 12:39:42 +0200

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,118 @@
-import React from "react";
+// import React, { useState } from "react";
+// import { ROUTES } from "../../config/router";
+// import { useNavigate } from "react-router-dom";
+// import { useModalWindow } from "hooks/useModalWindow";
+// import { Modal } from "components/Modal/Modal";
+// import SignUp from "components/SignUp";
+// import SignIn from "components/SignIn";
+// import { NavMenuWrapper, NavMenuLink } from "./NavBar.styled";
+
+// const NavBar = ({ inverse }) => {
+//   const navigate = useNavigate();
+
+//   const [isLogin, setIsLogin] = useState(true);
+//   const { isOpen, open, close } = useModalWindow();
+//   const switchToSignIn = () => setIsLogin(true);
+//   const switchToSignUp = () => setIsLogin(false);
+
+//   const token = "";
+
+//   const onClose = () => {
+//     close();
+//   };
+
+//   const addRecipe = () => {
+//     if (token) {
+//       navigate("/recipe/add");
+//     } else {
+//       open();
+//     }
+//   };
+
+//   return (
+//     <>
+//       <NavMenuWrapper>
+//         <NavMenuLink
+//           to={ROUTES.HOME}
+//           $inverse={inverse}
+//         >
+//           Home
+//         </NavMenuLink>
+//         <NavMenuLink
+//           onClick={addRecipe}
+//           $inverse={inverse}
+//         >
+//           Add recipe
+//         </NavMenuLink>
+//       </NavMenuWrapper>
+//       {isOpen && (
+//         <Modal onClose={onClose}>
+//           {isLogin ? (
+//             <SignIn switchForm={switchToSignUp} />
+//           ) : (
+//             <SignUp switchForm={switchToSignIn} />
+//           )}
+//         </Modal>
+//       )}
+//     </>
+//   );
+// };
+
+// export default NavBar;
+import React, { useState } from "react";
 import { ROUTES } from "../../config/router";
+import { useNavigate } from "react-router-dom";
+import { useModalWindow } from "hooks/useModalWindow";
+import { Modal } from "components/Modal/Modal";
+import SignUp from "components/SignUp";
+import SignIn from "components/SignIn";
 import { NavMenuWrapper, NavMenuLink } from "./NavBar.styled";
 
 const NavBar = ({ inverse }) => {
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(true);
+  const { isOpen, open, close } = useModalWindow();
+  const signIn = () => setIsLogin(true);
+  const signUp = () => setIsLogin(false);
+
+  const token = "";
+
+  const onClose = () => {
+    close();
+  };
+
+  const addRecipe = e => {
+    e.preventDefault();
+    if (token) {
+      navigate("/recipe/add");
+    } else {
+      open();
+    }
+  };
+
   return (
-    <NavMenuWrapper>
-      <NavMenuLink
-        to={ROUTES.HOME}
-        $inverse={inverse}
-      >
-        Home
-      </NavMenuLink>
-      <NavMenuLink
-        to={ROUTES.RECIPE_ADD}
-        $inverse={inverse}
-      >
-        Add recipe
-      </NavMenuLink>
-    </NavMenuWrapper>
+    <>
+      <NavMenuWrapper>
+        <NavMenuLink
+          to={ROUTES.HOME}
+          $inverse={inverse}
+        >
+          Home
+        </NavMenuLink>
+        <NavMenuLink
+          to={ROUTES.ADD_RECIPE}
+          onClick={addRecipe}
+          $inverse={inverse}
+        >
+          Add recipe
+        </NavMenuLink>
+      </NavMenuWrapper>
+      {isOpen && (
+        <Modal onClose={onClose}>
+          {isLogin ? <SignIn switchForm={signUp} /> : <SignUp switchForm={signIn} />}
+        </Modal>
+      )}
+    </>
   );
 };
 
