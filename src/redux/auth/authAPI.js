f5a7297e7217e2ecc87157f44780a73e822a7cb9
Serhiy Korytskyy <korysergey55@gmail.com>
2024-06-11 15:37:39 +0300

    remuve-base-url

--- src/redux/auth/authAPI.js
+++ src/redux/auth/authAPI.js
@@ -1,17 +1,16 @@
 import axios from "axios";
-const BASE_URL = "https://";
 
 export const authSignUpAPI = async data => {
-  const response = await axios.post(`${BASE_URL}/`, data);
+  const response = await axios.post(``, data);
   return response;
 };
 
 export const authSignInAPI = async data => {
-  const response = await axios.post(`${BASE_URL}/`, data);
+  const response = await axios.post(``, data);
   return response;
 };
 
 export const authLogOutAPI = async token => {
-  const response = await axios.post(`${BASE_URL}/`, token);
+  const response = await axios.post(``, token);
   return response;
 };
