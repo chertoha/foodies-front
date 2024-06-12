28b82fc7564ccc310dfe65f33ad90660a6d6380b
Serhiy Korytskyy <korysergey55@gmail.com>
2024-06-12 14:44:04 +0300

    deploy-ref

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { authSignInThunk } from "../../redux/auth/thunks";
 
