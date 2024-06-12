7e0dbe7722e3c79070db2eb611f8b5296f068910
Anton <a.chertok@akatech.com.ua>
2024-06-12 11:00:05 +0100

    fix: del header

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,13 +1,11 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import Header from "components/Header";
 import { FooterWrapper, Main, RootWrapper } from "./SharedLayout.styled";
 import Footer from "components/Footer";
 
 const SharedLayout = () => {
   return (
     <RootWrapper>
-      <Header />
       <Main>
         <Suspense>
           <Outlet />
